7f4dbcf1a9e00b5d58e7f938b8e57a95539411fb
cow8 <haffman@live.com>
2018-05-09 15:03:41 +0800

    remove invalid comment

--- viterbi.py
+++ viterbi.py
@@ -1,35 +1,8 @@
 # encoding=utf-8
-# """
-#
-# Created on Thu Feb 16 19:28:39 2017
-# 2017-4-2
-#     ForwardBackwardAlg函数功能：实现前向算法
-#     理论依据：李航《统计学习方法》
-# 2017-4-5
-#     修改了ForwardBackwardAlg函数名称为ForwardAlgo以及输出的alpha数组形式
-#     完成了BackwardAlgo函数功能：后向算法
-#     以及函数FBAlgoAppli：计算在观测序列和模型参数确定的情况下，
-#     某一个隐含状态对应相应的观测状态的概率
-# 2017-4-6
-#     完成BaumWelchAlgo函数一次迭代
-# 2017-4-7
-#     实现维特比算法
-# @author: sgp
-# """
 
 import numpy as np
 from collections import deque
 
-
-#
-# def one_word_fre(O, pinyin_list, fre, word_table_inverse):
-#     x = pinyin_list[O]
-#     ans = []
-#     for i in x:
-#         ans.append((word_table_inverse[i], fre[i]))
-#     sorted(ans, key=lambda s: s[1], reverse=True)
-#
-#     return ans
 
 class Node(object):
     def __init__(self, hanzi_code):
